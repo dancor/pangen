711755bad12fe2e7b52b74e9906ab5befeb56ee0
dan corson <danl@pima.(none)>
2008-10-06 10:53:51 -0700

    initial commit for pangen, doesn't compile yet

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,49 @@
 module Main where
 
+import Control.Arrow
+
+-- types are raw like Int, or take type params like 
+data Type = Type [String]
+
+Type = Var Int | FType Type Type | Datum String [Type] | BaseType String
+Func = Func String Type
+
+-- todo: parse human readable id :: a -> a to this
+-- note: we are assuming all List's will matchingly have one var etc..
+--    not sure if we'll add consistency checking or where
+-- note: no typeclasses (just Int, no Integral)
+-- note: by convention we always number left low
+--    but we could relax this for algorithmic simplicity or efficiency..
+funcs :: [Func]
+funcs = [
+  Func "id" (FType (Var 1) (Var 1)),
+  Func "const" (FType (Var 1) (FType (Var 2) (Var 1))),
+  Func "flip" (FType (FType (Var 1) (FType (Var 2) (Var 3))) 
+    (FType (Var 2) (FType (Var 1) (Var 3)))),
+  Func "(.)" (FType (FType (Var 1) (Var 2)) 
+    (FType (FType (Var 2) (Var 3)) (FType (Var 1) (Var 3)))),
+  Func "repeat" (FType (Var 1) (Datum "List" [Var 1])),
+  Func "map" (FType (FType (Var 1) (Var 2)) 
+    (FType (Datum "List" [Var 1]) (Datum "List" [Var 2]))),
+  Func "(+)" (FType (BaseType "Int") (FType (BaseType "Int") (BaseType "Int")))
+  ]
+
+funcMap = fromListWith (++) $ map (second (:[]) . f) funcs where
+  f (Func name (FType t1 t2)) = (t1, (t2, name))
+
+genExpr :: Type -> Rand g [String]
+genExpr (BaseType t) = case t of
+  "Int" -> return "1" -- todo: some variety..
+  _ -> error $ "don't know how to generate vals for base type: " ++ t
+genExpr (Datum t vars) = case t of
+  "List" -> return "Empty"  -- todo: variety
+  _ -> error $ "don't know how to generate vals for data type: " ++ t
+genExpr (Var 1) = genExpr (BaseType "Int") -- todo: variety
+genExpr (Var _) = error "renumbering failure.."
+genExpr (FType t1 t2) = 
+
 main :: IO ()
 main = do
+  gen <- getStdGen
+  genF gen 
   putStrLn "hi"
